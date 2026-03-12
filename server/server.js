1c2a28667256f57b5a1a565358bb52516b41e0a9
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2026-03-12 16:49:20 +0530

    style: remove extra blank line in config and add debug log
    Add console.log to output process.env.PORT for debugging environment variable loading. Also remove an unnecessary blank line in the configuration file for consistency.

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ import config from "./src/configs/config.js";
 
 const port = config.PORT;
 
+console.log(process.env.PORT);
+
 app.listen(port, () => {
   console.log(`Server is running on port http://localhost:${port}`);
 });
