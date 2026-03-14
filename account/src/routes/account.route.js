5379f554250524c4ae5af8611e1485a773b9c08e
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2026-03-14 21:01:06 +0530

    feat(account): add user endpoint to account routes
    Expose user information via a new /user endpoint, requiring authentication like other account routes.

--- account/src/routes/account.route.js
+++ account/src/routes/account.route.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   createAccount,
   getBalance,
+  user,
 } from "../controllers/account.controller.js";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 
@@ -9,5 +10,6 @@ const router = express.Router();
 
 router.post("/createAccount", authMiddleware, createAccount);
 router.get("/getBalance", authMiddleware, getBalance);
+router.get("/user", authMiddleware, user);
 
 export default router;
