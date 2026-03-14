52b9ad4393695e5d65fd8f22e7b8c756aed5cef9
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2026-03-14 17:03:38 +0530

    build: add cookie-parser dependency to account service
    Add cookie-parser middleware to parse incoming cookies in the Express application. This enables handling of cookie-based authentication and session management.

--- account/src/app.js
+++ account/src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import cookieParser from "cookie-parser";
 import cors from "cors";
 import indexRoute from "./routes/index.route.js";
 const app = express();
@@ -7,6 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+app.use(cookieParser());
 app.use(cors("*"));
 
 app.use("/api", indexRoute);
